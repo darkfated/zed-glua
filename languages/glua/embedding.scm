c89c14f8fab31f2c6b6ee2f10b9f64754e049603
darkfated <darkfated@icloud.com>
2026-07-30 22:53:45 +0300

    Fix for embedding

--- languages/glua/embedding.scm
+++ languages/glua/embedding.scm
@@ -1,20 +1,12 @@
-; Function declarations with leading comments (covers both `function` and `local function`)
-((comment) @context
-  .
-  (function_declaration
-    "function" @name
-    name: (_) @name
-    (comment) @collapse
-    body: (block) @collapse) @item)
+(function_declaration
+  "function" @name
+  name: (_) @name
+  body: (block) @collapse) @item
 
-; Variable-assigned functions with leading comments
-((comment) @context
-  .
-  (variable_declaration
-    (assignment_statement
-      (variable_list
-        (identifier) @name)
-      (expression_list
-        (function_definition
-          (comment) @collapse
-          body: (block) @collapse)))) @item)
+(variable_declaration
+  (assignment_statement
+    (variable_list
+      (identifier) @name)
+    (expression_list
+      (function_definition
+        body: (block) @collapse)))) @item
