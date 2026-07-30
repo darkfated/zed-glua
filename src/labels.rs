0768dce25ccf37a87eb37f43a051526a2cdbdae7
darkfated <darkfated@icloud.com>
2026-07-30 08:06:57 +0300

    Improve LSP completion labels rendering

--- src/labels.rs
+++ src/labels.rs
@@ -2,23 +2,31 @@ use zed_extension_api::lsp::{Completion, CompletionKind, Symbol, SymbolKind};
 use zed_extension_api::{CodeLabel, CodeLabelSpan};
 
 pub fn label_for_completion(completion: Completion) -> Option<CodeLabel> {
-    match completion.kind? {
-        CompletionKind::Method | CompletionKind::Function => {
-            let name_len = completion.label.find('(').unwrap_or(completion.label.len());
+    let kind = completion.kind?;
+
+    let label = completion.label;
+    let name_len = label.find('(').unwrap_or(label.len());
+
+    match kind {
+        CompletionKind::Method | CompletionKind::Function => Some(CodeLabel {
+            spans: vec![CodeLabelSpan::code_range(0..label.len())],
+            filter_range: (0..name_len).into(),
+            code: label,
+        }),
+
+        CompletionKind::Field => {
+            let len = label.len();
+
             Some(CodeLabel {
-                spans: vec![CodeLabelSpan::code_range(0..completion.label.len())],
-                filter_range: (0..name_len).into(),
-                code: completion.label,
+                spans: vec![CodeLabelSpan::literal(
+                    label.clone(),
+                    Some("property".into()),
+                )],
+                filter_range: (0..len).into(),
+                code: Default::default(),
             })
         }
-        CompletionKind::Field => Some(CodeLabel {
-            spans: vec![CodeLabelSpan::literal(
-                completion.label.clone(),
-                Some("property".into()),
-            )],
-            filter_range: (0..completion.label.len()).into(),
-            code: Default::default(),
-        }),
+
         _ => None,
     }
 }
